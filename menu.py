5a5a7c5177a591b531c9af70d7e3a49e1485352f
Bruno Bilhar Karaziack <brunobk1260@outlook.com>
2023-06-29 23:40:09 -0300

    Construindo um menu principal para o controle

---
+++ menu.py
@@ -0,0 +1,30 @@
+import krpc
+import time
+
+from screen import Screen
+
+def main():
+    conn = krpc.connect(name='rocket controller')
+    screen = Screen(conn)
+    screen_size = conn.ui.stock_canvas.rect_transform.size
+    screen.creat_screen([150, 190], (260-(screen_size[0]/2), (screen_size[1]/2)-200))
+
+    button_size = [140, 40]
+    amount_of_buttons = 0
+
+    screen.add_button("Orbit launch", True, button_size, [0, 50-(button_size[1] * amount_of_buttons)])
+    amount_of_buttons += 1
+    screen.add_button("in development 1...", False, button_size, [0, 50-(button_size[1] * amount_of_buttons)])
+    amount_of_buttons += 1
+    screen.add_button("in development 2...", False, button_size, [0, 50-(button_size[1] * amount_of_buttons)])
+    amount_of_buttons += 1
+    screen.add_button("in development 3...", False, button_size, [0, 50-(button_size[1] * amount_of_buttons)])
+    amount_of_buttons += 1
+
+    while 1:
+        if screen.get_state_of_button("Orbit launch"):
+            print("Orbit launch")
+
+        time.sleep(.1)
+
+main()
